e32bd88971f7339474bb014661761d860bd5673c
Ruslanlandiel117 <ruslan.lendiel@nure.ua>
2019-11-05 23:31:07 +0200

    delete

--- usermanagement_db/src/main/java/db/ConnectionFactory.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-import db.exceptions.DatabaseExeption;
-
-import java.sql.Connection;
-
-public interface ConnectionFactory {
-    Connection createConnection() throws DatabaseExeption;
-}
