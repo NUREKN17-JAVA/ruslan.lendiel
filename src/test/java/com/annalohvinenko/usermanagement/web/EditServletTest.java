98026e3a85cadb459b87cfb14465553e65b8ddda
Ruslanlendiel117 <ruslan.lendiel@nure.ua>
2019-12-22 12:46:54 +0300

    We created the servlet and wrote the test for editing

---
+++ src/test/java/com/annalohvinenko/usermanagement/web/EditServletTest.java
@@ -0,0 +1,28 @@
+package com.annalohvinenko.usermanagement.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import com.annalohvinenko.usermanagement.User;
+
+public class EditServletTest extends MockServletTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        createServlet(EditServlet.class);
+    }
+
+    public void testEdit() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        getMockUserDao().expect("update", user);
+        addRequestParameter("id", user.getId().toString());
+        addRequestParameter("firstName", user.getFirstName());
+        addRequestParameter("lastName", user.getLastName());
+        addRequestParameter("dateOfBirth", DateFormat.getDateInstance().format(user.getDateOfBirth()));
+        addRequestParameter("ok", "Ok");
+        doPost();
+    }
+
+    
+}
