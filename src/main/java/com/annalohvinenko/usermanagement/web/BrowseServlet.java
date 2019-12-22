a126c97eea0c5caa0e2c7cd9e213c66dc227b0ca
Ruslanlendiel117 <ruslan.lendiel@nure.ua>
2019-12-22 12:09:22 +0300

    We began to write the implementation of servet

--- src/main/java/com/annalohvinenko/usermanagement/web/BrowseServlet.java
+++ src/main/java/com/annalohvinenko/usermanagement/web/BrowseServlet.java
@@ -1,7 +1,76 @@
 package com.annalohvinenko.usermanagement.web;
 
+import com.annalohvinenko.usermanagement.User;
+import com.annalohvinenko.usermanagement.db.DaoFactory;
+import com.annalohvinenko.usermanagement.db.DatabaseException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
 
 public class BrowseServlet extends HttpServlet {
 
+    private static final long serialVersionUID = -8030261012558457487L;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("add") != null) {
+            add(req, resp);
+        } else if (req.getParameter("edit") != null) {
+            edit(req, resp);
+        } else if (req.getParameter("delete") != null) {
+            delete(req, resp);
+        } else if (req.getParameter("details") != null) {
+            details(req, resp);
+        } else {
+            browse(req, resp);
+        }
+    }
+
+    private void delete(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Collection<User> users;
+        try {
+            users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession().setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (DatabaseException e) {
+            throw new ServletException(e);
+        }
+    }
+    
+    private void details(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("id") == null) {
+            req.setAttribute("error", "Select a user.");
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+            return;
+        }
+        try {
+            User user = DaoFactory.getInstance().getUserDao().find(Long.valueOf(req.getParameter("id")));
+            req.getSession().setAttribute("user", user);
+        } catch (DatabaseException e) {
+            req.setAttribute("error", "Error in the database: " + e.getMessage());
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+            return;
+        }
+        req.getRequestDispatcher("/details").forward(req, resp);
+    }
+    
 }
